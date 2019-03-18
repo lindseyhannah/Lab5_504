7ac14d24f5fb9d34562e08d75949fc481e551547
lindseyhannah <44279695+lindseyhannah@users.noreply.github.com>
2019-03-18 14:25:32 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
-var map = L.map('map')
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	setView: ([47.24, -122.44]),
-	setZoom: 11,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+    //create map variable and set the zoom/coordinates.  Add to map tile 
+	var map = new L.map("map", {
+		center:new L.LatLng(47.24, -122.44),
+		zoom: 11
+	});
+    map.addLayer(OpenStreetMap_DE);
