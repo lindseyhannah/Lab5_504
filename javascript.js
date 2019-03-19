40105f5f75815e093d864850c09b76965ea5f0e7
lindseyhannah <44279695+lindseyhannah@users.noreply.github.com>
2019-03-19 00:18:55 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,17 +1,18 @@
 window.onload = function(){
 alert("Click anywhere on the map to set a start and end point for routing");
 }
-var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+	//set the map tile
+	var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	});
     //create map variable and set the zoom/coordinates.  Add to map tile 
 	var map = new L.map("map", {
 		center:new L.LatLng(47.24, -122.44),
 		zoom: 11
 	});
     map.addLayer(OpenStreetMap_DE);
-
+    //Leaflet Routing controls and geocoder
     var control= L.Routing.control({
         waypoints: [
         	null
@@ -24,50 +25,55 @@ var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmd
         router: L.Routing.mapbox('pk.eyJ1IjoibG1oYW5uYWgiLCJhIjoiY2p0ZXEwbXlhMWtzcTN5b2I3MDVyNG00MCJ9.Sajhgv5OHLfXBiyZdRb9IA'),
         units: 'imperial',
     }).addTo(map);
-
+//Creates the button for the start and end point setting 
 function createButton(label, container) {
     var btn = L.DomUtil.create('button', '', container);
     btn.setAttribute('type', 'button');
     btn.innerHTML = label;
     return btn;
-}
+};
 
-
+//function that resets view to geolocation 
 function pointhome(e){
 		    
 		var home= map.locate({
-		    setView: true, 
-		    maxZoom: 16, 
+		    setView: true,  
 		    timeout: 15000, 
 		    watch: false,
 		    });
-		
-		L.DomEvent.on('click', function() {
-		             L.marker(e.latlng).addTo(map)
-		             	.bindPopup("You are here")
-		             map.closePopup();
-		});
 
 };
-
+//function that when map.locate is intiiated, places a circle icon and popup at user location
+map.on('locationfound', function(e){
+	L.circle(e.latlng).addTo(map);
+	var geoloc= L.popup()
+		.setContent("You are here")
+		.setLatLng(e.latlng)
+		.addTo(map);
+});
+//Sets start and end button functions to place waypoints at clicked location
 map.on('click', function(e) {
 
     var container = L.DomUtil.create('div'),
 
         startBtn = createButton('Start from this location', container),
         destBtn = createButton('Go to this location', container);
-		L.DomEvent.on(startBtn, 'click', function() {
+
+		L.popup()
+        	.setContent(container)
+       		.setLatLng(e.latlng)
+        	.openOn(map);
+
+        L.DomEvent.on(startBtn, 'click', function() {
 		             control.spliceWaypoints(0, 1, e.latlng);
 		             map.closePopup();
 		         });
+
 		L.DomEvent.on(destBtn, 'click', function() {
 		             control.spliceWaypoints(-1, 1, e.latlng);
 		             map.closePopup();
 		         });
- 	    L.popup()
-        	.setContent(container)
-       		.setLatLng(e.latlng)
-        	.openOn(map);
+ 	    
 });
 
 
